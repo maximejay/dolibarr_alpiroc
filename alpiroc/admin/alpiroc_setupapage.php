550e14ff33ef29d3769e6fb8b95a88889f0f480b
maximejay <maximejayallemand@gmail.com>
2016-12-31 16:14:57 +0000

    FIX include main failed if the module is installed into the custom directory

--- alpiroc/admin/alpiroc_setupapage.php
+++ alpiroc/admin/alpiroc_setupapage.php
@@ -39,6 +39,7 @@
 $res=0;
 if (! $res && file_exists("../main.inc.php")) $res=@include '../main.inc.php';					// to work if your module directory is into dolibarr root htdocs directory
 if (! $res && file_exists("../../main.inc.php")) $res=@include '../../main.inc.php';			// to work if your module directory is into a subdir of root htdocs directory
+if (! $res && file_exists("../../../main.inc.php")) $res=@include '../../../main.inc.php';			// to work if your module directory is into the subdir custom
 if (! $res && file_exists("../../../dolibarr/htdocs/main.inc.php")) $res=@include '../../../dolibarr/htdocs/main.inc.php';     // Used on dev env only
 if (! $res && file_exists("../../../../dolibarr/htdocs/main.inc.php")) $res=@include '../../../../dolibarr/htdocs/main.inc.php';   // Used on dev env only
 if (! $res) die("Include of main fails");
